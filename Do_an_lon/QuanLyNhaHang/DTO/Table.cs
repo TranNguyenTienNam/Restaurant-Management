5526c41da490c87850dbbf8e80088f7a05ada877
tiendat19521345 <71374867+tiendat19521345@users.noreply.github.com>
2020-12-02 21:38:20 +0700

    bài mới nhất (từ trước đến nay nha)

---
+++ Do_an_lon/QuanLyNhaHang/DTO/Table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaHang
+{
+    public class Table
+    {
+        private int maban;
+        private string tenban;
+        private string trangthai;
+        public Table(DataRow data)
+        {
+            this.Maban = (int)data["MABAN"];
+            this.Trangthai = (string)data["TRANGTHAI"];
+            this.Tenban= (string)data["TENBAN"];
+
+        }
+        public Table() { }
+
+
+        public int Maban { get => maban; set => maban = value; }
+        public string Tenban { get => tenban; set => tenban = value; }
+        public string Trangthai { get => trangthai; set => trangthai = value; }
+    }
+}
